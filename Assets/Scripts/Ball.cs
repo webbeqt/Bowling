6c45b5a3b0841363ece4aaba2e782c7af74de68f
webbE <anton.wahlberg@edelan.se>
2017-03-21 21:51:54 +0100

    Created Ball script and CameraControl

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Vector3 launchVelocity;
+
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = launchVelocity;
+
+        Launch();
+
+    }
+
+    private void Launch()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Ball ball;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+        // Find offset
+        offset = transform.position - ball.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(transform.position.z < 1829)
+        {
+            transform.position = ball.transform.position + offset;
+        }
+
+	}
+}
